2477721328a342e21b0660a2f2ac0d15e3c31318
Brian Bolander <bbolander@vidder.com>
2016-02-11 17:33:16 -0800

    Added one liner for cli process monitoring.

--- bash/bash_one_liners.sh
+++ bash/bash_one_liners.sh
@@ -3,7 +3,12 @@
 #
 ###############################################################################
 
-# Change all the filenames in a directory tree that start with a capital letter
-# to a lowercase letter.
+#   Change all the filenames in a directory tree that start with a capital letter
+#   to a lowercase letter.
+#
 for file in `find testdir -type f`; do echo $file; new_f_name=`echo $file | sed 's/\(.*\/\)\(.\)\(.*\)$/\1\L\2\E\3/'`; echo $new_f_name; mv $file $new_f_name; done
+
+#   Monitor processes in a loop from the command line.
+#
+while true; do echo -en "\n\n-- TIME: ";date; ps -aef | grep -v grep | egrep "(supervisor|vidder-server-agent|tomcat7)"; sleep 5; done
 
